3ddc647237c46deb931d03a27fc6a738d6f95245
Lucas Martins <44577287+Claus7777@users.noreply.github.com>
2024-08-17 23:58:03 -0300

    GMTK is your duty!!

--- tronco.gd
+++ tronco.gd
@@ -1,8 +1,12 @@
 extends TileMapLayer
 
 @export var health_points = 10
+var particle_emitter
+var audio_player
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	particle_emitter = $GPUParticles2D
+	audio_player = $AudioStreamPlayer2D
 	pass # Replace with function body.
 
 
@@ -14,4 +18,6 @@ func take_damage(damage):
 	health_points -= damage
 	if health_points <= 0:
 		queue_free()
-	
+	else: 
+		particle_emitter.emitting = true
+		audio_player.play()
